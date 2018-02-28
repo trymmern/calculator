a4e413a1d5752d822eced59eadbaa8252ed59815
Trym Røsand Todalshaug <trymrt@gmail.com>
2018-02-28 20:30:29 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,40 @@
 import { Component } from '@angular/core';
+import * as math from 'mathjs';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <img width="300" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://github.com/angular/angular-cli/wiki">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+    scInput1:number = 0; // First number in simple calc
+    scOperator:string = ''; // Operator for simple calc
+    scInput2:number = 0; // Second number in simple calc
+    acInput:string = ''; // String for advanced calc 
+
+    scOutput:number;
+    acOutput:number;
+
+    calc() {
+        console.log(this.scOperator);
+
+        if (this.scOperator == "+") this.scOutput = this.scInput1 + this.scInput2;
+        
+        else if (this.scOperator == "-") this.scOutput = this.scInput1 - this.scInput2;
+
+        else if (this.scOperator == "*") this.scOutput = this.scInput1 * this.scInput2;
+
+        else if (this.scOperator == "/") this.scOutput = this.scInput1 / this.scInput2;
+
+        else console.log('Error: Choose an operator');
+
+        return this.scOutput;
+    }
+
+    advCalc() {
+        this.acOutput = math.eval(this.acInput);
+        return this.acOutput;
+    }
 }
